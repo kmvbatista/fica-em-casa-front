27f30c455c02b4a33292250cc7ca152d5a9dd2b8
Kennedy <kennedymessias.vb@gmail.com>
2020-04-05 01:50:04 -0300

    choose-group: texto do centro

--- src/pages/ChooseGroup/index.js
+++ src/pages/ChooseGroup/index.js
@@ -1,18 +1,50 @@
 import React from 'react';
-import { Container, Box1, Box2, CenteredBox, SubTitle, Title } from './styles';
+import {
+  Container,
+  Box1,
+  Box2,
+  CenteredBox,
+  TitleContainer,
+  SubTitle,
+  Title,
+  HighlightText,
+  SecondaryText,
+} from './styles';
 
 export default function ChooseGroup() {
   return (
     <Container>
       <Box1>
-        <div>
-          <Title></Title>
-          <SubTitle></SubTitle>
-        </div>
-        <CenteredBox></CenteredBox>
+        <TitleContainer>
+          <Title>
+            Faço parte do<br></br>
+          </Title>
+          <SubTitle>grupo de risco</SubTitle>
+        </TitleContainer>
+        <CenteredBox>
+          <div style={{ direction: 'rtl' }}>
+            <HighlightText>
+              Preciso<br></br>
+            </HighlightText>
+            <SecondaryText>de ajuda</SecondaryText>
+          </div>
+        </CenteredBox>
       </Box1>
       <Box2>
-        <CenteredBox></CenteredBox>
+        <TitleContainer>
+          <Title>
+            Não faço parte<br></br>
+          </Title>
+          <SubTitle>do grupo de risco</SubTitle>
+        </TitleContainer>
+        <CenteredBox>
+          <div style={{ direction: 'rtl' }}>
+            <HighlightText>
+              Quero<br></br>
+            </HighlightText>
+            <SecondaryText>ajudar</SecondaryText>
+          </div>
+        </CenteredBox>
       </Box2>
     </Container>
   );
--- src/pages/ChooseGroup/styles.js
+++ src/pages/ChooseGroup/styles.js
@@ -6,17 +6,29 @@ export const Container = styled.div`
   min-height: 100vh;
 `;
 
+export const TitleContainer = styled.div`
+  padding-bottom: 2em;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
 export const Title = styled.text`
-  font-weight: bold;
+  font-weight: bolder;
+  font-size: 2em;
   color: white;
+  text-align: center;
 `;
 
 export const SubTitle = styled.text`
   color: white;
+  font-size: 2em;
+  font-weight: 500;
 `;
 
 export const Box1 = styled.div`
   display: flex;
+  flex-direction: column;
   box-sizing: content-box;
   height: 50vh;
   padding-bottom: 2em;
@@ -26,6 +38,8 @@ export const Box1 = styled.div`
 `;
 
 export const Box2 = styled.div`
+  flex-direction: column;
+
   display: flex;
   height: 50vh;
   transform: translate(0, -2em);
@@ -41,4 +55,19 @@ export const CenteredBox = styled.div`
   width: 70%;
   height: 24vh;
   border-radius: 30px;
+  align-items: center;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const HighlightText = styled.text`
+  color: var(--color-pink);
+  font-size: 3em;
+  font-weight: bolder;
+`;
+
+export const SecondaryText = styled.text`
+  color: var(--color-pink);
+  font-size: 2.2em;
+  font-weight: 400;
 `;

2a04d8ee9a94ec07658df26824ce2b14973d3a43
Kennedy <kennedymessias.vb@gmail.com>
2020-04-05 16:47:25 -0300

    global components

---
+++ src/globalComponents.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const PinkContainer = styled.div`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  background-color: var(--color-pink);
+  padding: 6em;
+  justify-content: space-around;
+`;

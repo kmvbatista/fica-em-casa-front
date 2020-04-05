22d881077d503988c74905be1a76178cb61558c3
Kennedy <kennedymessias.vb@gmail.com>
2020-04-05 19:52:04 -0300

    pink container responsivity

--- src/globalComponents.js
+++ src/globalComponents.js
@@ -5,6 +5,12 @@ export const PinkContainer = styled.div`
   min-height: 100vh;
   flex-direction: column;
   background-color: var(--color-pink);
-  padding: 6em 4em;
   justify-content: space-around;
+  padding: 6em 4em;
+  @media only screen and (min-width: 1400px) {
+    padding: 6em 10em;
+  }
+  @media only screen and (min-width: 1800px) {
+    padding: 6em 20em;
+  }
 `;

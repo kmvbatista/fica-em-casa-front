2ab6c7684acd92284dbea8c03052880fcea93212
Kennedy <kennedymessias.vb@gmail.com>
2020-04-05 17:45:48 -0300

    fixing versions eror

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,6 @@
 import React, { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 const Login = lazy(() => import('../pages/Login/index'));
-const Signup = lazy(() => import('../pages/Signup/index'));
 const ChoseGroup = lazy(() => import('../pages/ChooseGroup/index'));
 const NeedHelpForm = lazy(() => import('../pages/NeedHelpForm/index'));
 const NeedHelpOptions = lazy(() => import('../pages/NeedHelpOptions/index'));
@@ -12,9 +11,6 @@ export default function Routes() {
       <Suspense fallback={<div>Loading...</div>}>
         <Route exact path='/'>
           <Login></Login>
-        </Route>
-        <Route exact path='/signUp'>
-          <Signup></Signup>
         </Route>
         <Route exact path='/choose-group'>
           <ChoseGroup></ChoseGroup>

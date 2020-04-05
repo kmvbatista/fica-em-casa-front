a7dc409541a8b837ba1a9663e73040cee28e9a39
Kennedy <kennedymessias.vb@gmail.com>
2020-04-05 12:32:48 -0300

    checkbox has children working

--- src/pages/NeedHelpForm/index.js
+++ src/pages/NeedHelpForm/index.js
@@ -1,8 +1,49 @@
 import React from 'react';
-import {Container} from './styles'
+import { Container, Title, InputBlock, Input, Span } from './styles';
+import './styles.css';
 
 export default function NeedHelpForm() {
-    return (
-      <Container></Container>
-    );
+  return (
+    <Container>
+      <Title>
+        <p>Se você faz parte do grupo de risco, preencha abaixo:</p>
+      </Title>
+      <form>
+        <div>
+          <InputBlock style={{ flexDirection: 'column' }}>
+            <p>Quando você nasceu?</p>
+            <div style={{ display: 'flex' }}>
+              <Input type='number' />
+              <Input type='number' />
+              <Input type='number' />
+            </div>
+          </InputBlock>
+        </div>
+        <InputBlock>
+          <p>Você tem filhos?</p>
+          <div style={{ position: 'relative' }}>
+            <input type='radio' name='children' id='sim' style={{}} />
+            <Span>sim</Span>
+          </div>
+          <div style={{ position: 'relative', marginLeft: '3.5em' }}>
+            <input type='radio' name='children' id='nao' style={{}} />
+            <label htmlFor='nao'></label>
+            <Span>não</Span>
+          </div>
+        </InputBlock>
+        <InputBlock>
+          <p>Quantos?</p>
+          <Input type='number' />
+        </InputBlock>
+        <InputBlock>
+          <p>Faixa Etária?</p>
+          <Input type='text' />
+        </InputBlock>
+        <InputBlock>
+          <p>Quantos moram com você?</p>
+          <Input type='number' />
+        </InputBlock>
+      </form>
+    </Container>
+  );
 }
--- src/pages/NeedHelpForm/styles.js
+++ src/pages/NeedHelpForm/styles.js
@@ -6,4 +6,39 @@ export const Container = styled.div`
   flex-direction: column;
   background-color: var(--color-pink);
   padding: 3em;
-`
+  justify-content: space-around;
+`;
+
+export const Title = styled.div`
+  color: white;
+  font-size: 2em;
+  font-weight: bolder;
+`;
+
+export const InputBlock = styled.div`
+  margin-bottom: 2em;
+  display: flex;
+  font-size: 1.7em;
+  color: white;
+`;
+
+export const Input = styled.input`
+  border: white 2px solid;
+  border-radius: 10px;
+  background-color: var(--color-pink);
+  width: 4em;
+  height: 4em;
+  margin-right: 1em;
+`;
+
+export const Span = styled.span`
+  position: absolute;
+  display: inline-block;
+  border: white 2px solid;
+  border-radius: 10px;
+  background-color: transparent;
+  width: 3.5em;
+  height: 3.5em;
+  margin-right: 1em;
+  z-index: 20;
+`;

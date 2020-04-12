09c0d72d3e9a97370057226bf2b82eded7ed82a6
Kennedy <kennedymessias.vb@gmail.com>
2020-04-12 17:47:07 -0300

    setting up .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,7 @@
 import axios from 'axios';
 
-// return AsyncStorage.getItem('@BatteryCollector:token').then( token => {
-//   const instance = axios.create({baseURL:'https://turnitgreenerapiback.azurewebsites.net',
-//   headers: {
-//       'Content-Type': 'application/json',
-//   }});
-//   instance.interceptors.request.use(function (config) {
-//       const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiMyIsImV4cCI6MTU2OTg3NzczNCwiaXNzIjoia2VubmVkeUJhdHRlcnlQcm9qZWN0IiwiYXVkIjoiQmF0dGVyeUNvbGxlY3RvclVzZXJzIn0.dkwQN3KBLmYqWubSS0huafj8RAq5lCWFARD_JCP-Kkw'
-//       config.headers.Authorization =  token ? `Bearer ${token}` : '';
-//       return config;
-//   });
-//   return instance;
-
 const instance = axios.create({
-  baseURL: 'https://ficaemcasa-backend.herokuapp.com/',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 instance.interceptors.request.use(function (config) {

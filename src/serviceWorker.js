d7766a75349bfd7f490721baf78105642f8c1a09
Kennedy <kennedymessias.vb@gmail.com>
2020-04-12 13:53:26 -0300

    deploying service worker config

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,13 +1,13 @@
 export const registerServiceWorker = () => {
   if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
-    window.addEventListener('load', function() {
+    window.addEventListener('load', function () {
       navigator.serviceWorker
         .register('/sw.js')
-        .then(function(registration) {
+        .then(function (registration) {
           // Registration was successful
           //console.log('ServiceWorker registration successful with scope: ', registration.scope);
         })
-        .catch(function(err) {
+        .catch(function (err) {
           // registration failed :(
           //console.log('ServiceWorker registration failed: ', err);
         });

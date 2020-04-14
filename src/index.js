39844a95f13a7ecf5c53b2f5bdbc2a187c24a1bc
Kennedy <kennedymessias.vb@gmail.com>
2020-04-13 21:46:48 -0300

    handling backend requests on canHelpOptions

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import './assets/fonts/BalooThambi2-Regular.ttf';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,4 +12,4 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
-serviceWorker.registerServiceWorker();
+// serviceWorker.registerServiceWorker();
--- src/pages/CanHelpOptions/index.js
+++ src/pages/CanHelpOptions/index.js
@@ -7,27 +7,87 @@ import {
 } from '../../globalComponents';
 import { SubTitle, Title } from './styles';
 import cardData from '../../assets/productCategory.json';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import * as AssistanceService from '../../services/assistanceService';
+import swal from 'sweetalert';
+import Loading from 'react-loading';
+import IsChecked from '../NeedHelpOptions/isChecked';
 
 export default function NeedHelpOptions() {
+  const [cards, setCards] = useState([]);
+  useEffect(() => {
+    getCards();
+  }, []);
+
+  const getCards = () => {
+    const dataWithLoading = cardData.map((x) => {
+      x.isLoading = false;
+      return x;
+    });
+    setCards(dataWithLoading);
+  };
+
+  const toggleIsCardChecked = (category) => {
+    const index = cards.findIndex((x) => x.category == category);
+    cards[index].isChecked = !cards[index].isChecked;
+    setCards([...cards]);
+  };
+
+  const toggleCardLoading = (category) => {
+    const index = cards.findIndex((x) => x.category == category);
+    cards[index].isLoading = !cards[index].isLoading;
+    setCards([...cards]);
+  };
+
+  const postCategoryAssistance = async (category) => {
+    toggleCardLoading(category);
+    try {
+      AssistanceService.postAssistance(
+        category,
+        toggleCardLoading,
+        toggleIsCardChecked,
+      );
+    } catch (error) {
+      swal(
+        'Não foi possível adotar a categoria ' + category + '!',
+        'Por favor, tente novamente',
+        error,
+      );
+    }
+  };
+
   return (
     <ColumnContainer style={{ backgroundColor: 'var(--color-purple)' }}>
       <Title>Quero ajudar</Title>
       <SubTitle>
+        Escolha as categorias nas quais você pode ajudar.
+        <br />
         Pode escolher mais de uma opção, tá?
-        <br />É muito importante que você descreva como pode ajudar após a
-        seleção da categoria, assim fica mais fácil de ajudar!
       </SubTitle>
       <Grid>
-        {cardData.map((el) => (
+        {cards.map((el) => (
           <OptionCard
             key={el.category}
             style={{ color: 'var(--color-purple)' }}
+            onClick={() => postCategoryAssistance(el.category)}
           >
-            <img
-              src={el.imageUrl}
-              alt={el.category}
-              style={{ maxHeight: '55%' }}
-            />
+            <IsChecked isChecked={el.isChecked}></IsChecked>
+            {el.isLoading ? (
+              <Loading
+                height='30%'
+                width='30%'
+                type='spinningBubbles'
+                color='var(--color-purple)'
+              ></Loading>
+            ) : (
+              <img
+                src={el.imageUrl}
+                alt={el.category}
+                style={{ maxHeight: '55%' }}
+              />
+            )}
+
             {el.category}
           </OptionCard>
         ))}

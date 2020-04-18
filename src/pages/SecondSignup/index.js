0c60db376552157cbee1c379d5522204de25741c
Kennedy <kennedymessias.vb@gmail.com>
2020-04-18 19:07:53 -0300

    second form

--- src/pages/SecondSignup/index.js
+++ src/pages/SecondSignup/index.js
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import Wait from './Components/Wait';
 
-import SecondForm from './Components/SecondForm/SecondForm';
+import {
+  Welcome,
+  LoginInput,
+  RegisterButton,
+  Title,
+  InitialForm,
+  Container,
+} from '../FirstSignup/styles';
 // import { useEffect } from 'react';
 
 export default function SignIn() {
@@ -25,17 +31,75 @@ export default function SignIn() {
   };
 
   return (
-    <div style={{ padding: 0 }}>
-      <Wait></Wait>
-      <SecondForm
-        phone={phone}
-        setPhone={setPhone}
-        password={password}
-        setPassword={setPassword}
-        confirmPassword={confirmPassword}
-        setConfirmPassword={setConfirmPassword}
-        handleSubmit={handleSubmit}
-      ></SecondForm>
-    </div>
+    <Container>
+      <Welcome
+        style={{
+          position: 'relative',
+          zIndex: '-1',
+        }}
+      >
+        <img
+          src='./people-at-the-window.png'
+          alt='people'
+          style={{
+            position: 'absolute',
+            right: '0%',
+            height: '57%',
+          }}
+        />
+        <img
+          src='./person-at-the-window.png'
+          alt='person'
+          style={{
+            position: 'absolute',
+            height: '50%',
+            left: '0',
+            top: '3em',
+            zIndex: '-1',
+            filter: 'opacity(0.5)',
+          }}
+        />
+        <Title>
+          <strong
+            style={{
+              fontSize: '2em',
+              marginLeft: '1em',
+              color: 'white',
+            }}
+          >
+            Quase lá!
+          </strong>
+        </Title>
+      </Welcome>
+      <InitialForm>
+        <LoginInput
+          placeholder='Seu telefone'
+          name='telefone'
+          id='telefone'
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          required
+        ></LoginInput>
+        <LoginInput
+          placeholder='Criar senha'
+          type='password'
+          name='password'
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></LoginInput>
+        <LoginInput
+          placeholder='Confirmar senha'
+          type='password'
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          name='confirmPassword'
+          required
+        ></LoginInput>
+        <RegisterButton onClick={() => handleSubmit()}>
+          Cadastrar
+        </RegisterButton>
+      </InitialForm>
+    </Container>
   );
 }

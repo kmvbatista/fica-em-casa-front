4fcfd7101cfa5b61d9e210b443ffc869bfd0686d
Kennedy <kennedymessias.vb@gmail.com>
2020-04-21 01:35:45 -0300

    sending confirmation code outbones

--- src/pages/FirstSignup/index.js
+++ src/pages/FirstSignup/index.js
@@ -10,15 +10,25 @@ import {
   LoginInput,
   RegisterButton,
 } from './styles';
+import swal from 'sweetalert';
+import InputMask from 'react-input-mask';
 
 export default function Login() {
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
+  const [confirmation, setConfirmation] = useState('');
+  const [isConfirmming, setConfirmming] = useState(false);
 
   const history = useHistory();
 
-  const handleSubmit = async () => {
-    history.push('second-signup', { name, phone });
+  const handleSubmit = async () => {};
+  const handleContinue = async () => {
+    swal(
+      `${
+        name.split(' ')[0]
+      }, vamos enviar um código de confirmação para o número ${phone}!`,
+      'Será por sms, ok?!',
+    ).then((x) => setConfirmming(true));
   };
 
   return (
@@ -31,28 +41,49 @@ export default function Login() {
         </Subtitle>
       </Welcome>
       <InitialForm>
-        <Title>Faça o seu cadastro para continuar...</Title>
-
-        <LoginInput
-          placeholder='seu nome'
-          name='name'
-          id='name'
-          required
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        ></LoginInput>
-        <LoginInput
-          onChange={(e) => setPhone(e.target.value)}
-          placeholder='seu telefone'
-          name='tel'
-          id='tel'
-          type='tel'
-          value={phone}
-          required
-        ></LoginInput>
-        <RegisterButton onClick={() => handleSubmit()}>
-          cadastrar-se
-        </RegisterButton>
+        {isConfirmming ? (
+          <Title>Insira o código enviado por favor..</Title>
+        ) : (
+          <Title>Faça o seu cadastro para continuar...</Title>
+        )}
+        {isConfirmming ? (
+          <InputMask
+            mask='9 9 9 9'
+            value={confirmation}
+            onChange={(e) => setConfirmation(e.target.value)}
+          >
+            {(inputProps) => (
+              <LoginInput {...inputProps} placeholder='_ _ _ _' />
+            )}
+          </InputMask>
+        ) : (
+          <>
+            <LoginInput
+              placeholder='seu nome'
+              name='name'
+              id='name'
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></LoginInput>
+            <LoginInput
+              onChange={(e) => setPhone(e.target.value)}
+              placeholder='seu telefone'
+              name='tel'
+              id='tel'
+              type='tel'
+              value={phone}
+              required
+            ></LoginInput>
+          </>
+        )}
+        {isConfirmming ? (
+          <RegisterButton onClick={handleSubmit}>
+            Confirmar número
+          </RegisterButton>
+        ) : (
+          <RegisterButton onClick={handleContinue}>continuar</RegisterButton>
+        )}
       </InitialForm>
     </Container>
   );

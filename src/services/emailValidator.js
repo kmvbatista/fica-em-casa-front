b18a296874260e662d8bb637f8b75ffd3330e7d3
Kennedy <kennedymessias.vb@gmail.com>
2020-04-25 20:36:04 -0300

    fix: regex email validator

--- src/services/emailValidator.js
+++ src/services/emailValidator.js
@@ -1,3 +1,8 @@
-function isEmailValid(email) {
-  return;
+export function isEmailValid(email) {
+  return (
+    email &&
+    email.match(
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    )
+  );
 }
